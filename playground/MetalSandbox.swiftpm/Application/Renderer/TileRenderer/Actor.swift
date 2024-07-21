ca1050ea5962d45a3d6b6db088e2d5f693563382
a-nakano <oracle02k@gmail.com>
2024-07-21 15:33:37 +0000

    added TileRenderer.

---
+++ playground/MetalSandbox.swiftpm/Application/Renderer/TileRenderer/Actor.swift
@@ -0,0 +1,15 @@
+import MetalKit
+
+class Actor {
+    let color: vector_float4
+    let position: vector_float3
+    let rotation: vector_float3
+    let scale: vector_float3
+    
+    init(color: vector_float4, position: vector_float3, rotation: vector_float3, scale: vector_float3) {
+        self.color = color;
+        self.position = position;
+        self.rotation = rotation;
+        self.scale = scale;
+    }
+}
